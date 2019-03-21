1fa4577a595bd1d76f80dae01209d3829937e7b4
Rahul Vyas <rahulvyas@plaak.com>
2019-03-21 14:23:50 +0530

    login completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container } from 'semantic-ui-react';
+import Login from './Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+  
+        <Container>
+          <Login></Login>
+        </Container>
+ 
     );
   }
 }
